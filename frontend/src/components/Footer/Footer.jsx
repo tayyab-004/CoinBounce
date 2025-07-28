26a4c2e3cf9b31c788096bb207137d493ac2acda
tayyab-004 <tayyabgee333@gmail.com>
2025-07-28 16:22:18 +0500

    Update Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import styles from "./Footer.module.css";
 function Footer() {
-  return <p className={styles.footer}>&copy; CoinBounce 2023-24</p>;
+  return <p className={styles.footer}>&copy; CoinBounce 2020-{new Date().getFullYear()}</p>;
 }
 
 export default Footer;
